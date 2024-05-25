695c5b0d820562f53e08081136a892a4c2644bff
JP Sherka <jp.sherka@gmail.com>
2024-05-24 22:25:11 -0400

    add components

--- src/page/page.jsx
+++ src/page/page.jsx
@@ -1,11 +1,30 @@
-import DataDrawer from "../components/DataDrawer";
+import Typography from '@mui/material/Typography';
+import NavBar from '../components/NavBar';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 
+const onDownload = () => {
+    const link = document.createElement("a");
+    link.download = `download.txt`;
+    link.href = "./download.txt";
+    link.click();
+  };
 
 const Page = () => {
+
     return (
         <div>
-            <DataDrawer/>
-            <h1> Hola </h1>
+            <NavBar/>
+            <Box>
+                <Typography variant="h2" gutterBottom>
+                    Hola este es mi portafolio
+                </Typography>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </Box>
+            <Button  onClick={onDownload} variant="contained" color="primary">
+                Descarga mi CV aqui!
+            </Button>
         </div>
     )
 };
