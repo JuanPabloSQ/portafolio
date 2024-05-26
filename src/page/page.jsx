9c86bb408a76952c4a7e08046d3baf65f1688bb1
JP Sherka <jp.sherka@gmail.com>
2024-05-25 20:01:36 -0400

    clean appbar and fixed position

--- src/page/page.jsx
+++ src/page/page.jsx
@@ -4,29 +4,54 @@ import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 
-const onDownload = () => {
-    const link = document.createElement("a");
-    link.download = `download.txt`;
-    link.href = "./download.txt";
-    link.click();
-  };
-
 const Page = () => {
-
     return (
         <div>
             <NavBar/>
-            <Box>
-                <Typography variant="h2" gutterBottom>
-                    Hola este es mi portafolio
-                </Typography>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    minHeight: '100vh',
+                    padding: 2,
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        gap: 2,
+                        mb: 2,
+                    }}
+                >
+                    <Box>
+                    <Typography variant="h2" gutterBottom>
+                        Hola, Soy Juan Pablo SQ.
+                    </Typography>
+                    <Typography variant="h4" gutterBottom>
+                        Desarollador Full-Stack / Nurse
+                    </Typography>
+                    <Typography variant="h5" gutterBottom>
+                        Y este es mi portafolio
+                    </Typography>
+                    </Box>
+                    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{ width: 300, height: 300 }} />
+                </Box>
+                <Button
+                    variant="contained"
+                    color="info"
+                    href="/resume.pdf"
+                    download
+                >
+                    <Typography variant='h5'> Descarga mi CV </Typography>
+                </Button>
             </Box>
-            <Button  onClick={onDownload} variant="contained" color="primary">
-                Descarga mi CV aqui!
-            </Button>
         </div>
-    )
+    );
 };
 
-export default Page
+export default Page;
