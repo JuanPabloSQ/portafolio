18ef955aed8c832a2623a63a23724779adb723f3
JP Sherka <jp.sherka@gmail.com>
2024-05-29 16:41:16 -0400

    config vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.webp'], // Incluir archivos .webp en el análisis de activos
+  assetsInclude: ['**/*.webp'], 
 });
